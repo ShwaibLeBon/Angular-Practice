772ced3e92c1188bbd2f74d348fa279bd5fa8cbc
ArtcalO <niyca14@gmail.com>
2023-05-19 09:15:06 +0200

    add invoice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,18 @@ export class AppComponent {
   onInvoicesView: boolean = true;
 
   handleSelectedItem(item: any): void {
-    this.selectedItems.push({ ...item, quantity: 1 });
+    if(this.selectedItems.length==0){
+        this.selectedItems.push({ ...item, quantity: 1 })
+      }else{
+        let itemIdx = this.selectedItems.findIndex((x:any)=> x.id==item.id)
+        if(itemIdx>-1){
+          this.selectedItems[itemIdx].quantity+=1
+        }else{
+          this.selectedItems.push({ ...item, quantity: 1 })
+        }
+      }
     this.onInvoicesView = false;
   }
-
   handlePerformedEvent() {
     this.onInvoicesView = true;
     this.selectedItems = [];
